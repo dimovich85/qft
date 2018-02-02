a8ef82583cd1c5796c7e329beffd9fc8c2e8d9a1
dimovich85 <dimovich85@gmail.com>
2018-02-02 20:22:15 +0200

    Сделал анимацию на кнопке вниз. Сделал кнопку наверх. Сделал плавный скролл по якорям. Сделал параллакс эффект. Сделал ховеры. Сделал модальные окна. Осталось поправить адаптивку, сделать меню гамбургер.

--- js/js.js
+++ js/js.js
@@ -1,5 +1,114 @@
+$(function(){
+
+/* Modal */
+
+let $modalContainer = $('section.modal-container');
+let classForClose = 'd-n';
+let activeContentClass = 'active';
+
+let $showContact = $('button.btn_contact');
+let $showVideo = $('button.btn_video');
+let $showMore = $('button.btn_more');
+
+let $modalContact = $('section.modal-container .contact-us');
+let $modalVideo = $('section.modal-container .watch-video');
+let $modalMore = $('section.modal-container .more');
+
+	$('.modal-container').on('click', function(e){
+		if ($(e.target).is('section.modal-container')){
+			$('section.modal-container .modal__content.active').removeClass(activeContentClass);
+			$modalContainer.addClass(classForClose);
+		}
+	});
+
+	$showContact.on('click', function(){
+		$modalContact.addClass(activeContentClass);
+		$modalContainer.removeClass(classForClose);
+	});
+
+	$showVideo.on('click', function(){
+		$modalVideo.addClass(activeContentClass);
+		$modalContainer.removeClass(classForClose);
+	});
+
+	$showMore.on('click', function(){
+		$modalMore.addClass(activeContentClass);
+		$modalContainer.removeClass(classForClose);
+	});
+
+/* END Modal */
+
+/* Logo link */
+
+	$('.header__nav figure.logotype').on('click', function(e){
+		e.preventDefault();
+		location.href = '#top';
+	});
+
+/* END Logo link */
+
+/* Links menu and scroll */
+
+let $links = $('.navblock__link, .link__down, .button-up')
+				.not(function(){
+					return $(this).hasClass('navblock__link_phone');
+				});
+
+	$links.on('click', function scroll(e){
+		e.preventDefault();
+		let offset = 100;
+		let $elem = $(e.target);
+		let link;
+		if( !$elem.is('img') ){
+			link =  $elem.attr('href');
+		} else{
+			link = ( $elem.find('a').attr('href') == undefined )
+					? $elem.parent('a').attr('href')
+					: $elem.find('a').attr('href');
+		}
+		$('html, body').animate({
+			scrollTop: $(link).offset().top - offset
+		}, 500);
+		return false;
+	});
+
+/* END Links */
+
+/* Parallax */
+
+	$(window).on('scroll', function(e){
+		let $scrollWindow = $(e.target);
+		let $target = $('main.first-screen');
+		let targetParentHeight = $target.parents('.header').outerHeight();
+		let scrolledPixels = $scrollWindow.scrollTop();
+		let offset = (scrolledPixels / targetParentHeight) * 100 / 5;
+		//console.log('targetParentHeight ' + targetParentHeight);
+		//console.log('scrolledPixels ' + scrolledPixels);
+
+		if(scrolledPixels < targetParentHeight + 30 ){
+			$target.css({
+				transition: 'transform 0.5s',
+				transform: `translateY(${offset}%) `
+			});
+			//console.log('offset ' + offset);
+		} else {
+			offset = 0;
+			$target.css({
+				transform: `translateY(${offset})`,
+				transition: 'transform 0.5s'
+			});
+			//console.log('offset ' + offset);
+		}
+	});
+
+/* Parallax */
+
+});
+
+
+
 // Работа с окнами (показ, скрытие)
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('img.closer').click(function(){
 		$('.cont_menu').addClass('closed');
 		$('.join_form').addClass('closed');
@@ -17,21 +126,21 @@ $(document).ready(function(){
 	$('button.connect-us').click(function(){
 		$('.join_form').removeClass('closed');
 	});
-});
+});*/
 
 // Плавный скролл
 
-function scroll(id){
+/*function scroll(id){
 	var offset = 100;
 	$('html, body').animate({
 		scrollTop: $(id).offset().top - offset
 	}, 500);
 	return false;
-}
+}*/
 
 // Paralax
 
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$(window).scroll(function(){
 		var position = $(this).scrollTop();
 		console.log(position);
@@ -54,11 +163,11 @@ $(document).ready(function(){
 		
 	});
 
-});
+});*/
 
 // Header
 
-$(window).scroll(function(){
+/*$(window).scroll(function(){
 	var position = $(this).scrollTop();
 	if (position >= 700){
 		$('.first-screen header').addClass('scroll-header');
@@ -66,11 +175,11 @@ $(window).scroll(function(){
 	else{
 		$('.first-screen header.scroll-header').removeClass('scroll-header');
 	}
-});
+});*/
 
 // Button up
 
-$(window).scroll(function(){
+/*$(window).scroll(function(){
 	var position = $(this).scrollTop();
 	if (position >= 3100){
 		$('.button-up').removeClass('closed');
@@ -78,27 +187,27 @@ $(window).scroll(function(){
 	else{
 		$('.button-up').addClass('closed');
 	}
-});
+});*/
 
 // Header for table, fixed d-ostatok
-$('.table_container').on('scroll', function(e){
+/*$('.table_container').on('scroll', function(e){
 	$('thead').offset({
 		left: - e.target.scrollLeft + 10,
 	});
-});
+});*/
 
 
 // Работа с модальными окнами
 
-let $modalContainer = $('section.modal-container');
+/*let $modalContainer = $('section.modal-container');
 let classForClose = 'd-n';
 let activeContent = 'active';
-let $iconCloser = $('section.modal-container i.fa.closer');
+let $iconCloser = $('section.modal-container i.fa.closer');*/
 // Настройка кнопки закрытия модального окна
-	$iconCloser.on('click', function(){
+	/*$iconCloser.on('click', function(){
 		$(activeContent).removeClass('active');
 		$modalContainer.addClass(classForClose);
-	});
+	});*/
 // Для корректного отображения, надо сначала добавить в контент нужное содержимое
 // потом задать ему класс active
 // после чего убрать класс .d-n у section, а закрыть - наоборот
